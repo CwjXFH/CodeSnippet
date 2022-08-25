5021f5bb1ea989c92f4b6b8ffa3f4baa9b5faef7
eason <chiwenjun@outlook.com>
2022-08-25 14:44:58 +0800

    feat: add MemoryCache extension

---
+++ dotnet/Cache/src/MemoryCache/MemoryCacheExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System.Collections;
+using System.Reflection;
+
+namespace MemoryCache
+{
+    public static class MemoryCacheExtensions
+    {
+        public static int KeyCount(this IMemoryCache memoryCache)
+        {
+            if (memoryCache.GetType().GetProperty("Count")?.GetValue(memoryCache) is int count)
+            {
+                return count;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Return all memory cache keys, and expired time if it exists.
+        /// </summary>
+        public static IDictionary<string, DateTimeOffset?> GetAllKeys(this IMemoryCache memoryCache)
+        {
+            var entriesCollection = memoryCache.GetType().GetProperty("EntriesCollection", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetProperty)?.GetValue(memoryCache);
+
+
+            if (entriesCollection is not null and IDictionary cacheDict)
+            {
+                var keyInfoDict = new Dictionary<string, DateTimeOffset?>();
+                foreach (DictionaryEntry cache in cacheDict)
+                {
+                    if (cache.Value is ICacheEntry cacheEntry)
+                    {
+                        if (cacheEntry.Key is string cachekey && string.IsNullOrEmpty(cachekey) == false)
+                        {
+                            keyInfoDict[cachekey] = cacheEntry.AbsoluteExpiration;
+                        }
+                    }
+                }
+                return keyInfoDict;
+            }
+
+            return new Dictionary<string, DateTimeOffset?>(0);
+        }
+
+        public static void ClearAll(this IMemoryCache memoryCache)
+        {
+            var compact = memoryCache.GetType()
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase)
+                .Where(m => m.Name == "Compact")
+                .FirstOrDefault(m => m.GetParameters().Length == 1);
+            compact?.Invoke(memoryCache, new object[] { 1.0 });
+        }
+    }
+}
---
+++ dotnet/Cache/tests/MemoryCache.Test/MemoryCacheExtensionsTest.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MemoryCache.Test
+{
+    public class MemoryCacheExtensionsTest
+    {
+        private IServiceProvider _serviceProvider;
+        public MemoryCacheExtensionsTest()
+        {
+            _serviceProvider = new ServiceCollection().AddMemoryCache().BuildServiceProvider();
+        }
+
+        [Fact]
+        public void KeyCount_HasItems_ReturnTrue()
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var memoryCache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+            var count = 3;
+            for (var i = 0; i < count; i++)
+            {
+                memoryCache.Set($"{i}", i);
+            }
+
+
+            var keyCount = memoryCache.KeyCount();
+
+            Assert.Equal(count, keyCount);
+        }
+
+        [Fact]
+        public void KeyCount_Empty_ReturnTrue()
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var memoryCache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+
+            var keyCount = memoryCache.KeyCount();
+
+            Assert.Equal(0, keyCount);
+        }
+
+
+
+
+        [Fact]
+        public void GetAllKeys_HasItems_ReturnTrue()
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var memoryCache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+            var key = "key";
+            memoryCache.Set(key, 1);
+
+            var keys = memoryCache.GetAllKeys();
+
+            Assert.Equal(key, keys.First().Key);
+        }
+
+        [Fact]
+        public void GetAllKeys_Empty_ReturnTrue()
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var memoryCache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+
+            var keys = memoryCache.GetAllKeys();
+
+            Assert.Equal(0, keys.Count);
+        }
+
+        [Fact]
+        public void GetAllKeys_HasItemWithoutExpire_ReturnTrue()
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var memoryCache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+            var key = "key";
+            memoryCache.Set(key, 1);
+
+            var keys = memoryCache.GetAllKeys();
+
+            Assert.Null(keys.First().Value);
+        }
+
+        [Fact]
+        public void GetAllKeys_HasItemWithExpire_ReturnTrue()
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var memoryCache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+            var key = "key";
+            memoryCache.Set(key, 1, TimeSpan.FromSeconds(1));
+
+            var keys = memoryCache.GetAllKeys();
+
+            Assert.NotNull(keys.First().Value);
+        }
+
+
+
+        [Fact]
+        public void ClearAll_HasItems_ReturnTrue()
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var memoryCache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+            memoryCache.Set("1", 1);
+
+            memoryCache.ClearAll();
+            var count = memoryCache.KeyCount();
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void ClearAll_Empty_ReturnTrue()
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var memoryCache = scope.ServiceProvider.GetRequiredService<IMemoryCache>();
+
+            memoryCache.ClearAll();
+            var count = memoryCache.KeyCount();
+
+            Assert.Equal(0, count);
+        }
+
+    }
+}
