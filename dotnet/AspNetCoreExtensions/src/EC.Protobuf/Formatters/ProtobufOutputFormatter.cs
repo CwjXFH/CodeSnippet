16baf88698627f7ed03ba9dc96bf71e914e515c9
CwjXFH <chiwenjun@outlook.com>
2025-12-02 14:13:46 +0800

    refactor: ProtobufOutputFormatter performance

--- dotnet/AspNetCoreExtensions/src/EC.Protobuf/Formatters/ProtobufOutputFormatter.cs
+++ dotnet/AspNetCoreExtensions/src/EC.Protobuf/Formatters/ProtobufOutputFormatter.cs
@@ -1,46 +1,69 @@
-﻿using System.Text;
 using EC.Protobuf.Constants;
 using Google.Protobuf;
 using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace EC.Protobuf.Formatters;
 
-public class ProtobufOutputFormatter : TextOutputFormatter
+public class ProtobufOutputFormatter : IOutputFormatter
 {
+    private MediaTypeCollection _supportedMediaTypes { get; } = [];
+
     public ProtobufOutputFormatter()
     {
-        base.SupportedEncodings.Add(InternalEncoding.UTF8NoBOM);
-        base.SupportedEncodings.Add(InternalEncoding.UTF8WithBOM);
-        base.SupportedEncodings.Add(Encoding.Unicode);
-        base.SupportedMediaTypes.Add(HttpContentType.Application.Protobuf);
+        _supportedMediaTypes.Add(HttpContentType.Application.Protobuf);
     }
 
     public ProtobufOutputFormatter(IEnumerable<string> mediaTypes) : this()
     {
         foreach (var type in mediaTypes)
         {
-            base.SupportedMediaTypes.Add(type);
+            _supportedMediaTypes.Add(type);
         }
     }
 
 
-    public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+    public bool CanWriteResult(OutputFormatterCanWriteContext context)
     {
-        var httpContext = context.HttpContext;
-        var selectedUtf8 = selectedEncoding.CodePage == Encoding.UTF8.CodePage;
-        var buffer = context.Object is IMessage message ? message.ToByteArray() : [];
-
-        if (selectedUtf8)
+        var contentTypeSupported = false;
+        if (context.ContentType.HasValue)
         {
-            var responseStream = httpContext.Response.Body;
-            await responseStream.WriteAsync(buffer);
-            await responseStream.FlushAsync(context.HttpContext.RequestAborted);
+            contentTypeSupported = _supportedMediaTypes.Any(mt =>
+                mt.Equals(context.ContentType.Value, StringComparison.OrdinalIgnoreCase));
+        }
+
+        if (contentTypeSupported == false)
+        {
+            return false;
+        }
+
+        if (context.Object is not IMessage && context.Object is not Stream)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public async Task WriteAsync(OutputFormatterWriteContext context)
+    {
+        var response = context.HttpContext.Response;
+
+        if (context.ContentType != null)
+        {
+            response.ContentType = context.ContentType.Value;
+        }
+
+        if (context.Object is IMessage message)
+        {
+            message.WriteTo(response.Body);
+            await response.Body.FlushAsync(context.HttpContext.RequestAborted);
             return;
         }
 
-        await using var transcodingStream =
-            Encoding.CreateTranscodingStream(httpContext.Response.Body, selectedEncoding, Encoding.UTF8, leaveOpen: true);
-        await transcodingStream.WriteAsync(buffer);
-        await transcodingStream.FlushAsync();
+        if (context.Object is Stream stream)
+        {
+            using var valueAsStream = stream;
+            await valueAsStream.CopyToAsync(response.Body, context.HttpContext.RequestAborted);
+        }
     }
 }
